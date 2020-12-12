31cdea7aa9e2a0d9c2d7fd16ad848d90249f7acd
Arash <75451920+4rash-uni@users.noreply.github.com>
2020-12-12 22:20:10 +0100

    Merged data and predicatealign models

--- code/pytorch/data_model.py
+++ code/pytorch/data_model.py
@@ -1,7 +1,13 @@
+import gc
+import multiprocessing
+
+import numpy as np
+import Levenshtein
+from sklearn import preprocessing
+
 from base.kgs import read_kgs_from_folder
 from pytorch.literal_encoder import encode_literals, literal_vectors_exists, load_literal_vectors, save_literal_vectors
-from pytorch.utils import read_local_name, clear_attribute_triples, read_word2vec, load_args
-from sklearn import preprocessing
+from pytorch.utils import task_divide, merge_dic, read_local_name, clear_attribute_triples, read_word2vec, l2_normalize, load_args
 
 
 def generate_sup_attribute_triples(sup_links, av_dict1, av_dict2):
@@ -20,20 +26,177 @@ def generate_sup_attribute_triples(sup_links, av_dict1, av_dict2):
 
 def generate_dict(literal_list, literal_vectors_list):
     assert len(literal_list) == len(literal_vectors_list)
-    dic = dict()
-    list()
+    dic = {}
     for i in range(len(literal_list)):
         dic[literal_list[i]] = literal_vectors_list[i]
     return dic
 
 
 def generate_literal_id_dic(literal_list):
-    literal_id_dic = dict()
+    literal_id_dic = {}
     print('literal id', len(literal_list), len(set(literal_list)))
     for i in range(len(literal_list)):
         literal_id_dic[literal_list[i]] = i
     assert len(literal_list) == len(literal_id_dic)
     return literal_id_dic
+
+
+def link2dic(links):
+    dic1, dic2 = {}, {}
+    for i, j, w in links:
+        dic1[i] = (j, w)
+        dic2[j] = (i, w)
+    assert len(dic1) == len(dic2)
+    return dic1, dic2
+
+
+def generate_sup_predicate_triples(predicate_links, triples1, triples2):
+    link_dic1, link_dic2 = link2dic(predicate_links)
+    sup_triples1, sup_triples2 = set(), set()
+    for s, p, o in triples1:
+        if p in link_dic1:
+            sup_triples1.add((s, link_dic1.get(p)[0], o, link_dic1.get(p)[1]))
+    for s, p, o in triples2:
+        if p in link_dic2:
+            sup_triples2.add((s, link_dic2.get(p)[0], o, link_dic2.get(p)[1]))
+    return list(sup_triples1), list(sup_triples2)
+
+
+def add_weights(predicate_links, triples1, triples2, min_w_before):
+    link_dic1, link_dic2 = link2dic(predicate_links)
+    weighted_triples1, weighted_triples2 = set(), set()
+    w = 0.2
+    for s, p, o in triples1:
+        if p in link_dic1:
+            weighted_triples1.add((s, p, o, zoom_weight(link_dic1.get(p)[1], min_w_before)))
+        else:
+            weighted_triples1.add((s, p, o, w))
+    for s, p, o in triples2:
+        if p in link_dic2:
+            weighted_triples2.add((s, p, o, zoom_weight(link_dic2.get(p)[1], min_w_before)))
+        else:
+            weighted_triples2.add((s, p, o, w))
+    assert len(triples1) == len(weighted_triples1)
+    assert len(triples2) == len(weighted_triples2)
+    return list(weighted_triples1), list(weighted_triples2), weighted_triples1, weighted_triples2
+
+
+def init_predicate_alignment(predicate_local_name_dict_1, predicate_local_name_dict_2, predicate_init_sim):
+    def get_predicate_match_dict(p_ln_dict_1, p_ln_dict_2):
+        predicate_match_dict, sim_dict = {}, {}
+        for p1, ln1 in p_ln_dict_1.items():
+            match_p2 = ''
+            max_sim = 0
+            for p2, ln2 in p_ln_dict_2.items():
+                sim_p2 = Levenshtein.ratio(ln1, ln2)
+                if sim_p2 > max_sim:
+                    match_p2 = p2
+                    max_sim = sim_p2
+            predicate_match_dict[p1] = match_p2
+            sim_dict[p1] = max_sim
+        return predicate_match_dict, sim_dict
+
+    match_dict_1_2, sim_dict_1 = get_predicate_match_dict(predicate_local_name_dict_1, predicate_local_name_dict_2)
+    match_dict_2_1, sim_dict_2 = get_predicate_match_dict(predicate_local_name_dict_2, predicate_local_name_dict_1)
+
+    predicate_match_pairs_set = set()
+    predicate_latent_match_pairs_similarity_dict = {}
+    for p1, p2 in match_dict_1_2.items():
+        if match_dict_2_1[p2] == p1:
+            predicate_latent_match_pairs_similarity_dict[(p1, p2)] = sim_dict_1[p1]
+            if sim_dict_1[p1] > predicate_init_sim:
+                predicate_match_pairs_set.add((p1, p2, sim_dict_1[p1]))
+                # print(p1, p2, sim_dict_1[p1], sim_dict_2[p2])
+    return predicate_match_pairs_set, predicate_latent_match_pairs_similarity_dict
+
+
+def read_predicate_local_name_file(file_path, relation_set):
+    relation_local_name_dict, attribute_local_name_dict = {}, {}
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            line = line.strip('\n').split('\t')
+            assert len(line) == 2
+            if line[0] in relation_set:
+                relation_local_name_dict[line[0]] = line[1]
+            else:
+                attribute_local_name_dict[line[0]] = line[1]
+    file.close()
+    return relation_local_name_dict, attribute_local_name_dict
+
+
+def predicate2id_matched_pairs(predicate_match_pairs_set, predicate_id_dict_1, predicate_id_dict_2):
+    id_match_pairs_set = set()
+    for (p1, p2, w) in predicate_match_pairs_set:
+        if p1 in predicate_id_dict_1 and p2 in predicate_id_dict_2:
+            id_match_pairs_set.add((predicate_id_dict_1[p1], predicate_id_dict_2[p2], w))
+    return id_match_pairs_set
+
+
+def find_predicate_alignment_by_embedding(embed, predicate_list1, predicate_list2, predicate_id_dict1, predicate_id_dict2):
+    embed = preprocessing.normalize(embed)
+    sim_mat = np.matmul(embed, embed.T)
+
+    matched_1, matched_2 = {}, {}
+    for i in predicate_list1:
+        sorted_sim = (-sim_mat[i, :]).argsort()
+        for j in sorted_sim:
+            if j in predicate_list2:
+                matched_1[i] = j
+                break
+    for j in predicate_list2:
+        sorted_sim = (-sim_mat[j, :]).argsort()
+        for i in sorted_sim:
+            if i in predicate_list1:
+                matched_2[j] = i
+                break
+
+    id_attr_dict1, id_attr_dict2 = {}, {}
+    for a, i in predicate_id_dict1.items():
+        id_attr_dict1[i] = a
+    for a, i in predicate_id_dict2.items():
+        id_attr_dict2[i] = a
+
+    predicate_latent_match_pairs_similarity_dict = {}
+    for i, j in matched_1.items():
+        if matched_2[j] == i:
+            predicate_latent_match_pairs_similarity_dict[(i, j)] = sim_mat[i, j]
+    return predicate_latent_match_pairs_similarity_dict
+
+
+def zoom_weight(weight, min_w_before, min_w_after=0.5):
+    weight_new = 1.0 - (1.0 - weight) * (1.0 - min_w_after) / (1.0 - min_w_before)
+    return weight_new
+
+
+def _generate_neighbours(entity_embeds, entity_list, neighbors_num, threads_num):
+    ent_frags = task_divide(entity_list, threads_num)
+    ent_frag_indexes = task_divide(np.arange(0, entity_list.shape[0]), threads_num)
+
+    pool = multiprocessing.Pool(processes=len(ent_frags))
+    results = []
+    for i in range(len(ent_frags)):
+        results.append(pool.apply_async(find_neighbours, args=(ent_frags[i], entity_list, entity_embeds[ent_frag_indexes[i], :], entity_embeds, neighbors_num)))
+    pool.close()
+    pool.join()
+
+    dic = {}
+    for res in results:
+        dic = merge_dic(dic, res.get())
+
+    del results
+    gc.collect()
+    return dic
+
+
+def find_neighbours(frags, entity_list, sub_embed, embed, k):
+    dic = {}
+    sim_mat = np.matmul(sub_embed, embed.T)
+    for i in range(sim_mat.shape[0]):
+        sort_index = np.argpartition(-sim_mat[i, :], k)
+        neighbors_index = sort_index[0:k]
+        neighbors = entity_list[neighbors_index].tolist()
+        dic[frags[i]] = neighbors
+    return dic
 
 
 class DataModel:
@@ -47,6 +210,27 @@ class DataModel:
         self._generate_literal_vectors()
         self._generate_name_vectors()
         self._generate_attribute_value_vectors()
+
+        self.relation_name_dict1, self.attribute_name_dict1 = read_predicate_local_name_file(
+            self.args.training_data + 'predicate_local_name_1', set(self.kgs.kg1.relations_id_dict.keys()))
+        self.relation_name_dict2, self.attribute_name_dict2 = read_predicate_local_name_file(
+            self.args.training_data + 'predicate_local_name_2', set(self.kgs.kg2.relations_id_dict.keys()))
+
+        self.relation_id_alignment_set = None
+        # self.train_relations1, self.train_relations2 = None, None
+
+        self.attribute_id_alignment_set = None
+        # self.train_attributes1, self.train_attributes2 = None, None
+
+        self.relation_alignment_set, self.relation_latent_match_pairs_similarity_dict_init = \
+            init_predicate_alignment(self.relation_name_dict1, self.relation_name_dict2, self.args.predicate_init_sim)
+        self.attribute_alignment_set, self.attribute_latent_match_pairs_similarity_dict_init = \
+            init_predicate_alignment(self.attribute_name_dict1, self.attribute_name_dict2, self.args.predicate_init_sim)
+        self.relation_alignment_set_init = self.relation_alignment_set
+        self.attribute_alignment_set_init = self.attribute_alignment_set
+        self.update_relation_triples(self.relation_alignment_set)
+        self.update_attribute_triples(self.attribute_alignment_set)
+        self.neighbors1, self.neighbors2 = {}, {}
 
     def _generate_literal_vectors(self):
 
@@ -66,7 +250,7 @@ class DataModel:
         self.literal_id_dic = generate_literal_id_dic(self.literal_list)
 
     def _generate_name_vectors(self):
-        name_ordered_list = list()
+        name_ordered_list = []
         num = len(self.entities)
         print("total entities:", num)
         entity_id_uris_dic = dict(zip(self.kgs.kg1.entities_id_dict.values(), self.kgs.kg1.entities_id_dict.keys()))
@@ -104,7 +288,7 @@ class DataModel:
                 values_set.add(v)
                 attribute_triples_list2.add((h, a, v))
         print("selected attribute triples", len(attribute_triples_list1), len(attribute_triples_list2))
-        values_id_dic = dict()
+        values_id_dic = {}
         values_list = list(values_set)
         num = len(values_list)
         for i in range(num):
@@ -117,7 +301,7 @@ class DataModel:
         self.kgs.kg1.add_sup_attribute_triples(sup_triples1)
         self.kgs.kg2.add_sup_attribute_triples(sup_triples2)
         num = len(values_id_dic)
-        value_ordered_list = list()
+        value_ordered_list = []
         for i in range(num):
             value = values_list[i]
             value_index = self.literal_id_dic.get(value)
@@ -128,6 +312,68 @@ class DataModel:
         if self.args.literal_normalize:
             value_vectors = preprocessing.normalize(value_vectors)
         self.value_vectors = value_vectors
+
+    def update_attribute_triples(self, attribute_alignment_set):
+        self.attribute_id_alignment_set = predicate2id_matched_pairs(attribute_alignment_set,
+                                                                     self.kgs.kg1.attributes_id_dict,
+                                                                     self.kgs.kg2.attributes_id_dict)
+        # self.train_attributes1 = [a for (a, _, _) in self.attribute_id_alignment_set]
+        # self.train_attributes2 = [a for (_, a, _) in self.attribute_id_alignment_set]
+        self.kgs.kg1.sup_attribute_alignment_triples, self.kgs.kg2.sup_attribute_alignment_triples = \
+            generate_sup_predicate_triples(self.attribute_id_alignment_set, self.kgs.kg1.local_attribute_triples_list,
+                                           self.kgs.kg2.local_attribute_triples_list)
+        self.kgs.kg1.attribute_triples_w_weights, self.kgs.kg2.attribute_triples_w_weights, self.kgs.kg1.attribute_triples_w_weights_set, \
+        self.kgs.kg2.attribute_triples_w_weights_set = add_weights(self.attribute_id_alignment_set,
+                                                            self.kgs.kg1.local_attribute_triples_list,
+                                                            self.kgs.kg2.local_attribute_triples_list,
+                                                            self.args.predicate_soft_sim)
+
+    def update_relation_triples(self, relation_alignment_set):
+        self.relation_id_alignment_set = predicate2id_matched_pairs(relation_alignment_set,
+                                                                    self.kgs.kg1.relations_id_dict,
+                                                                    self.kgs.kg2.relations_id_dict)
+        # self.train_relations1 = [a for (a, _, _) in self.relation_id_alignment_set]
+        # self.train_relations2 = [a for (_, a, _) in self.relation_id_alignment_set]
+        self.kgs.kg1.sup_relation_alignment_triples, self.kgs.kg2.sup_relation_alignment_triples = \
+            generate_sup_predicate_triples(self.relation_id_alignment_set, self.kgs.kg1.local_relation_triples_list,
+                                           self.kgs.kg2.local_relation_triples_list)
+        self.kgs.kg1.relation_triples_w_weights, self.kgs.kg2.relation_triples_w_weights, self.kgs.kg1.relation_triples_w_weights_set, \
+        self.kgs.kg1.relation_triples_w_weights_set = add_weights(self.relation_id_alignment_set,
+                                                           self.kgs.kg1.local_relation_triples_list,
+                                                           self.kgs.kg2.local_relation_triples_list,
+                                                           self.args.predicate_soft_sim)
+
+    def _update_predicate_alignment(self, embed, predicate_type='relation', w=0.7):
+        if predicate_type == 'relation':
+            predicate_list1, predicate_list2 = self.kgs.kg1.relations_list, self.kgs.kg2.relations_list
+            predicate_id_dict1, predicate_id_dict2 = self.kgs.kg1.relations_id_dict, self.kgs.kg2.relations_id_dict
+            predicate_alignment_set_init = self.relation_alignment_set_init
+        else:
+            predicate_list1, predicate_list2 = self.kgs.kg1.attributes_list, self.kgs.kg2.attributes_list
+            predicate_id_dict1, predicate_id_dict2 = self.kgs.kg1.attributes_id_dict, self.kgs.kg2.attributes_id_dict
+            predicate_alignment_set_init = self.attribute_alignment_set_init
+
+        predicate_latent_match_pairs_similarity_dict = \
+            find_predicate_alignment_by_embedding(embed, predicate_list1, predicate_list2, predicate_id_dict1,
+                                                  predicate_id_dict2)
+
+        predicate_alignment_set = set()
+        for (p1, p2, sim_init) in predicate_alignment_set_init:
+            p_id_1 = predicate_id_dict1[p1]
+            p_id_2 = predicate_id_dict2[p2]
+            sim = sim_init
+            if (p_id_1, p_id_2) in predicate_latent_match_pairs_similarity_dict:
+                sim = w * sim + (1 - w) * predicate_latent_match_pairs_similarity_dict[(p_id_1, p_id_2)]
+            if sim > self.args.predicate_soft_sim:
+                predicate_alignment_set.add((p1, p2, sim))
+        print('update ' + predicate_type + ' alignment:', len(predicate_alignment_set))
+
+        if predicate_type == 'relation':
+            self.relation_alignment_set = predicate_alignment_set
+            self.update_relation_triples(predicate_alignment_set)
+        else:
+            self.attribute_alignment_set = predicate_alignment_set
+            self.update_attribute_triples(predicate_alignment_set)
 
 
 if __name__ == '__main__':
