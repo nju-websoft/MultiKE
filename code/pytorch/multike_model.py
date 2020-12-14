43605951409bd137d6df0301ee3f67567197a4ec
Arash <75451920+4rash-uni@users.noreply.github.com>
2020-12-14 20:03:45 +0100

    Improved model and added embeds method

--- code/pytorch/multike_model.py
+++ code/pytorch/multike_model.py
@@ -1,5 +1,3 @@
-import gc
-
 import torch
 import torch.nn as nn
 import numpy as np
@@ -32,16 +30,16 @@ class Conv(nn.Module):
         x = torch.stack([attr_as, attr_vs], dim=1).unsqueeze(3)  # Nx2xDx1
         x_bn = self.bn(x.permute(0, 2, 1, 3)).permute(0, 3, 2, 1)
         x_conv = self.conv_block(x_bn)  # Nx2x2xD
-        x_conv_norm = l2_normalize(x_conv.permute(0, 2, 3, 1), dim=2)  # Nx2xDx2
-        x_fc = self.fc(x_conv_norm.flatten(1))
-        x_fc_norm = l2_normalize(x_fc)  # Important!!
-        score = -torch.sum(torch.square(attr_hs - x_fc_norm), dim=1)
+        x_conv_normed = l2_normalize(x_conv.permute(0, 2, 3, 1), dim=2)  # Nx2xDx2
+        x_fc = self.fc(x_conv_normed.flatten(1))
+        x_fc_normed = l2_normalize(x_fc)  # Important!!
+        score = -torch.sum(torch.square(attr_hs - x_fc_normed), dim=1)
         return score
 
 
 class MultiKENet(nn.Module):
 
-    def __init__(self, num_entities, num_relations, num_attributes, embed_dim, value_vectors, local_name_vectors):
+    def __init__(self, num_entities, num_relations, num_attributes, embed_dim, value_vectors, local_name_vectors, shared_space=False):
         super(MultiKENet, self).__init__()
         self.register_buffer('literal_embeds', torch.from_numpy(value_vectors), persistent=False)
         self.register_buffer('name_embeds', torch.from_numpy(local_name_vectors), persistent=False)
@@ -60,32 +58,24 @@ class MultiKENet(nn.Module):
         # Shared embeddings
         self.ent_embeds = nn.Parameter(torch.Tensor(num_entities, embed_dim))
 
-        # Shared combination
-        self.nv_mapping = nn.Parameter(torch.Tensor(embed_dim, embed_dim))
-        self.rv_mapping = nn.Parameter(torch.Tensor(embed_dim, embed_dim))
-        self.av_mapping = nn.Parameter(torch.Tensor(embed_dim, embed_dim))
-        self.register_buffer('eye', torch.eye(embed_dim), persistent=False)
+        self.cfg = {
+            # params, lookup
+            'rv': [(self.rv_ent_embeds, self.rel_embeds), self.relation_triple_lookup],
+            'av': [(self.av_ent_embeds, self.attr_embeds, self.attr_conv), self.attribute_triple_lookup],
+            'ckgrtv': [(self.rv_ent_embeds, self.rel_embeds), self.cross_kg_relation_triple_lookup],
+            'ckgatv': [(self.av_ent_embeds, self.attr_embeds, self.attr_triple_conv), self.cross_kg_attribute_triple_lookup],
+            'ckgrrv': [(self.rv_ent_embeds, self.rel_embeds), self.cross_kg_relation_reference_lookup],
+            'ckgarv': [(self.av_ent_embeds, self.attr_embeds, self.attr_ref_conv), self.cross_kg_attribute_reference_lookup],
+            'cnv': [(self.ent_embeds, self.rv_ent_embeds, self.av_ent_embeds), self.cross_name_view_lookup]
+        }
+        if shared_space:
+            # Shared combination
+            self.nv_mapping = nn.Parameter(torch.Tensor(embed_dim, embed_dim))
+            self.rv_mapping = nn.Parameter(torch.Tensor(embed_dim, embed_dim))
+            self.av_mapping = nn.Parameter(torch.Tensor(embed_dim, embed_dim))
+            self.register_buffer('eye', torch.eye(embed_dim), persistent=False)
 
-        self.lookups_cfg = {
-            'rv': self.relation_triple_lookup,
-            'av': self.attribute_triple_lookup,
-            'ckgrtv': self.cross_kg_relation_triple_lookup,
-            'ckgatv': self.cross_kg_attribute_triple_lookup,
-            'ckgrrv': self.cross_kg_relation_reference_lookup,
-            'ckgarv': self.cross_kg_attribute_reference_lookup,
-            'cnv': self.cross_name_view_lookup,
-            'mv': self.multi_view_entities_lookup
-        }
-        self.params_cfg = {
-            'rv': [self.rv_ent_embeds, self.rel_embeds],
-            'av': [self.av_ent_embeds, self.attr_embeds, self.attr_conv],
-            'ckgrtv': [self.rv_ent_embeds, self.rel_embeds],
-            'ckgatv': [self.av_ent_embeds, self.attr_embeds, self.attr_triple_conv],
-            'ckgrrv': [self.rv_ent_embeds, self.rel_embeds],
-            'ckgarv': [self.av_ent_embeds, self.attr_embeds, self.attr_ref_conv],
-            'cnv': [self.ent_embeds, self.rv_ent_embeds, self.av_ent_embeds],
-            'mv': [self.ent_embeds, self.nv_mapping, self.rv_mapping, self.av_mapping]
-        }
+            self.cfg['mv'] = [(self.ent_embeds, self.nv_mapping, self.rv_mapping, self.av_mapping), self.multi_view_entities_lookup]
 
         self._init_parameters()
 
@@ -98,7 +88,7 @@ class MultiKENet(nn.Module):
 
     def parameters(self, view, recurse=True):
         params = []
-        for param in self.params_cfg[view]:
+        for param in self.cfg[view][0]:
             if isinstance(param, nn.Module):
                 params += list(param.parameters(recurse=recurse))
             else:
@@ -122,7 +112,7 @@ class MultiKENet(nn.Module):
         attr_pas = torch.index_select(self.attr_embeds, dim=0, index=attr_pos_as)
         attr_pvs = torch.index_select(self.literal_embeds, dim=0, index=attr_pos_vs)
         pos_score = self.attr_conv(attr_phs, attr_pas, attr_pvs)
-        return pos_score
+        return pos_score,
 
     def cross_kg_relation_triple_lookup(self, ckge_rel_pos_hs, ckge_rel_pos_rs, ckge_rel_pos_ts):
         rv_ent_embeds = l2_normalize(self.rv_ent_embeds)
@@ -138,7 +128,7 @@ class MultiKENet(nn.Module):
         ckge_attr_pas = torch.index_select(self.attr_embeds, dim=0, index=ckge_attr_pos_as)
         ckge_attr_pvs = torch.index_select(self.literal_embeds, dim=0, index=ckge_attr_pos_vs)
         pos_score = self.attr_triple_conv(ckge_attr_phs, ckge_attr_pas, ckge_attr_pvs)
-        return pos_score
+        return pos_score,
 
     def cross_kg_relation_reference_lookup(self, ckgp_rel_pos_hs, ckgp_rel_pos_rs, ckgp_rel_pos_ts):
         rv_ent_embeds = l2_normalize(self.rv_ent_embeds)
@@ -153,8 +143,8 @@ class MultiKENet(nn.Module):
         ckga_attr_phs = torch.index_select(av_ent_embeds, dim=0, index=ckga_attr_pos_hs)
         ckga_attr_pas = torch.index_select(self.attr_embeds, dim=0, index=ckga_attr_pos_as)
         ckga_attr_pvs = torch.index_select(self.literal_embeds, dim=0, index=ckga_attr_pos_vs)
-        pos_score = self.attr_ref_conv(ckga_attr_phs, ckga_attr_pas, index=ckga_attr_pvs)
-        return pos_score
+        pos_score = self.attr_ref_conv(ckga_attr_phs, ckga_attr_pas, ckga_attr_pvs)
+        return pos_score,
 
     def cross_name_view_lookup(self, cn_hs):
         ent_embeds = l2_normalize(self.ent_embeds)
@@ -177,10 +167,11 @@ class MultiKENet(nn.Module):
         return final_ents, nv_ents, rv_ents, av_ents
 
     def forward(self, inputs, view):
-        return self.lookups_cfg[view](*inputs)
+        return self.cfg[view][1](*inputs)
 
     @staticmethod
-    def valid(model, kgs, embed_choice='avg', w=(1, 1, 1), top_k=[1], num_threads=1):
+    @torch.no_grad()
+    def valid(args, model, dataloader, embed_choice='avg', w=(1, 1, 1)):
         if embed_choice == 'nv':
             ent_embeds = model.name_embeds
         elif embed_choice == 'rv':
@@ -196,15 +187,22 @@ class MultiKENet(nn.Module):
         else:  # 'final'
             ent_embeds = l2_normalize(model.ent_embeds)
         print(embed_choice, 'valid results:')
-        embeds1 = ent_embeds[kgs.valid_entities1, ]
-        embeds2 = ent_embeds[kgs.valid_entities2 + kgs.test_entities2, ]
-        _, mrr_12 = valid(embeds1, embeds2, None, top_k, num_threads, normalize=True)
-        del embeds1, embeds2
-        gc.collect()
+        embeds = []
+        for entities in dataloader:
+            entities = entities.to(ent_embeds.device)
+            embeds.append(torch.index_select(ent_embeds, dim=0, index=entities).cpu())
+
+        embeds = torch.cat(embeds, dim=0).numpy()
+        num_kg1_ents = len(dataloader.dataset.kg1)
+        embeds1 = embeds[:num_kg1_ents]
+        embeds2 = embeds[num_kg1_ents:]
+        hits1_12, mrr_12 = valid(embeds1, embeds2, None, args.top_k, args.test_threads_num, normalize=True)
         return mrr_12
 
     @staticmethod
-    def test(model, kgs, embed_choice='avg', w=(1, 1, 1), top_k=[1], num_threads=1):
+    @torch.no_grad()
+    def test(args, model, dataloader, embed_choice='avg', w=(1, 1, 1)):
+        model.eval()
         if embed_choice == 'nv':
             ent_embeds = model.name_embeds
         elif embed_choice == 'rv':
@@ -220,12 +218,43 @@ class MultiKENet(nn.Module):
         else:  # 'final'
             ent_embeds = l2_normalize(model.ent_embeds)
         print(embed_choice, 'test results:')
-        embeds1 = ent_embeds[kgs.test_entities1, ]
-        embeds2 = ent_embeds[kgs.test_entities2, ]
-        _, mrr_12 = valid(embeds1, embeds2, None, top_k, num_threads, normalize=True)
-        del embeds1, embeds2
-        gc.collect()
+        embeds = []
+        for entities in dataloader:
+            entities = entities.to(ent_embeds.device)
+            embeds.append(torch.index_select(ent_embeds, dim=0, index=entities).cpu())
+
+        embeds = torch.cat(embeds, dim=0).numpy()
+        num_kg1_ents = len(dataloader.dataset.kg1)
+        embeds1 = embeds[:num_kg1_ents]
+        embeds2 = embeds[num_kg1_ents:]
+        hits1_12, mrr_12 = valid(embeds1, embeds2, None, args.top_k, args.test_threads_num, normalize=True)
         return mrr_12
+
+    @staticmethod
+    @torch.no_grad()
+    def embeds(model, dataloader, embed_choice='rv', w=(1, 1, 1)):
+        model.eval()
+        if embed_choice == 'nv':
+            ent_embeds = model.name_embeds
+        elif embed_choice == 'rv':
+            ent_embeds = l2_normalize(model.rv_ent_embeds)
+        elif embed_choice == 'av':
+            ent_embeds = l2_normalize(model.av_ent_embeds)
+        elif embed_choice == 'final':
+            ent_embeds = l2_normalize(model.ent_embeds)
+        elif embed_choice == 'avg':
+            ent_embeds = w[0] * model.name_embeds + \
+                         w[1] * l2_normalize(model.rv_ent_embeds) + \
+                         w[2] * l2_normalize(model.av_ent_embeds)
+        else:  # 'final'
+            ent_embeds = l2_normalize(model.ent_embeds)
+        embeds = []
+        for entities in dataloader:
+            entities = entities[0].to(ent_embeds.device)
+            embeds.append(torch.index_select(ent_embeds, dim=0, index=entities).cpu())
+
+        embeds = torch.cat(embeds, dim=0).numpy()
+        return embeds
 
 
 if __name__ == '__main__':
